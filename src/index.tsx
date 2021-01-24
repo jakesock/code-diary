6b5b3012c2325b406e71b4c8e93deed01422ad88
Jacob Thomas <jakesock@users.noreply.github.com>
2021-01-24 05:48:16 -0500

    🎉 Added markdown editor component!

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import 'bootswatch/dist/darkly/bootstrap.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
