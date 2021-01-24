9eac9b00313eac4134fe688847ab1db32e533f82
Jacob Thomas <jakesock@users.noreply.github.com>
2021-01-24 01:13:23 -0500

    More refactoring!

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-import bundle from './bundler';
+import CodeCell from './components/CodeCell';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
-  const initialValue = `for (let i = 1; i <= 100; i++) {
-  if (i % 15 == 0) console.log('FizzBuzz');
-  else if (i % 3 == 0) console.log('Fizz');
-  else if (i % 5 == 0) console.log('Buzz');
-  else console.log(i);
-}`;
-
   return (
     <div>
-      <CodeEditor
-        initialValue={initialValue}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
