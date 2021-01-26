5686d0b2a2db18591d9c37784046394c418002a7
Jacob Thomas <jakesock@users.noreply.github.com>
2021-01-26 16:44:59 -0500

    Added Cumulative Code bundling and a function to display elements using the editor

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -6,6 +6,7 @@ import Preview from './Preview';
 import Resizable from './Resizable';
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useCumulativeCode } from '../hooks/useCumulativeCode';
 
 interface CodeCellProps {
   cell: Cell;
@@ -14,22 +15,23 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cumulativeCode, cell.content, createBundle]);
 
   return (
     <Resizable direction="vertical">
